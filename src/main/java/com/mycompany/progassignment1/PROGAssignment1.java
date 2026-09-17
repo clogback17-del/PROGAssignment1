2176da358924c48d6739ed00516f2c5a644fec3e
letlhogonolokgosiemang <letlhogonolokgosiemang@localhost>
2026-09-17 20:59:40 +0200

    Initial project setup

---
+++ src/main/java/com/mycompany/progassignment1/PROGAssignment1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.progassignment1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author letlhogonolokgosiemang
+ */
+public class PROGAssignment1 {
+
+    public static void main(String[] args) {
+        
+    }
+}
