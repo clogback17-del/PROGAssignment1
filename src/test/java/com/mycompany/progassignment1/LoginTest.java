e838902a1a4bd5c94b7c9035f4d3f9fea5ccfcc0
letlhogonolokgosiemang <letlhogonolokgosiemang@localhost>
2026-09-18 22:55:20 +0200

    Added  unit tests for login logic

--- src/test/java/com/mycompany/progassignment1/LoginTest.java
+++ src/test/java/com/mycompany/progassignment1/LoginTest.java
@@ -34,6 +34,70 @@ public class LoginTest {
     
        
     }
+      
+      //password test
+      
+      @Test
+      public void testPasswordMeetsComplexity(){
+          assertTrue(login.checkPasswordComplexity("Ch&&sec@ke99!"));
+      }
+      
+      @Test
+      public void testPasswordCoesNotMeetComplexity(){
+          assertFalse(login.checkPasswordComplexity("password"));
+      }
+      
+      //Cellphone test
+      
+      @Test
+      public void testCellphoneCorrectlyFormatted(){
+          assertTrue(login.checkCellPhoneNumber("+2783896876"));
+      }
+      
+      @Test 
+      public void testCellPhoneIncorreclyFormatted(){
+          assertFalse(login.checkCellPhoneNumber("08966553"));
+      }
+      
+      //Registration message
+      
+      @Test
+      public void testRegisterUserSuccess(){
+          String expected = "Username successfully captured.\n"
+                + "Password successfully captured.\n"
+                + "Cell phone number successfully added.";
+        assertEquals(expected,
+                login.registerUser("kyl_1", "Ch&&sec@ke99!", "+27838968976", "Kyle", "Smith"));
+      }
+      
+      @Test
+      public void testRegisterUserBadUsernameMessage(){
+          String expected = "Username is not correctly formatted; please ensure that your username "
+                + "contains an underscore and is no more than five characters in length.";
+        assertEquals(expected,
+                login.registerUser("kyle!!!!!!!", "Ch&&sec@ke99!", "+27838968976", "Kyle", "Smith"));
+      }
+      
+       @Test
+    public void testRegisterUserBadPasswordMessage() {
+        String expected = "Password is not correctly formatted; please ensure that the password "
+                + "contains at least eight characters, a capital letter, a number, "
+                + "and a special character.";
+        assertEquals(expected,
+                login.registerUser("kyl_1", "password", "+27838968976", "Kyle", "Smith"));
+    }
+ 
+    @Test
+    public void testRegisterUserBadCellPhoneMessage() {
+        String expected = "Cell phone number incorrectly formatted or does not contain "
+                + "international code.";
+        assertEquals(expected,
+                login.registerUser("kyl_1", "Ch&&sec@ke99!", "08966553", "Kyle", "Smith"));
+    }
+    
+    // login test
+    
+    
     
     public LoginTest() {
     }
